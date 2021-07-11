add2c4403235db25a830e086467fecdc41ed682b
listanegra <vitormac3@hotmail.com>
2021-07-11 13:20:53 -0300

    Set default font to Roboto

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
             colors: {
                 'primary': '#4527A0'
             }
+        },
+        fontFamily: {
+            sans: ['Roboto', 'sans-serif']
         }
     },
     variants: {
